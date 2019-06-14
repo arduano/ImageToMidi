f6d4d57bbd868bcd44ded68d430497a055bab498
arduano <leonid.shchurov@gmail.com>
2019-06-14 21:29:49 +1000

    Finished basic layout

---
+++ ImageToMidi/ColorPicker16.xaml.cs
@@ -0,0 +1,109 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageToMidi
+{
+    /// <summary>
+    /// Interaction logic for ColorPicker16.xaml
+    /// </summary>
+
+    public partial class ColorPicker16 : UserControl
+    {
+        Color[] colors = new Color[16];
+        bool[] set = new bool[16];
+
+        public event Action PickStart;
+        public event Action PickStop;
+
+        public ColorPicker16()
+        {
+            InitializeComponent();
+            for(int i = 0; i < 16; i++)
+            {
+                getButton(i).Background = Brushes.Transparent;
+                colors[i] = Colors.Transparent;
+            }
+        }
+
+        int colorPickerButton = -1;
+        int lastPicker = -1;
+
+        Button getButton(int i)
+        {
+            return (Button)((Viewbox)buttonDock.Children[i]).Child;
+        }
+
+        int getButtonID(Button b)
+        {
+            return buttonDock.Children.IndexOf((Viewbox)b.Parent);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var b = (Button)sender;
+            int id = getButtonID(b);
+            if (set[id])
+            {
+                b.Background = Brushes.Transparent;
+                ((PackIcon)b.Content).Kind = PackIconKind.Eyedropper;
+                set[id] = false;
+            }
+            else
+            {
+                ((PackIcon)b.Content).Kind = PackIconKind.Vanish;
+                colorPickerButton = id;
+                lastPicker = id;
+                PickStart();
+            }
+        }
+
+        private void UserControl_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Height = ActualWidth / 16;
+        }
+
+        private void Button_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if(colorPickerButton != -1)
+            {
+                ((PackIcon)getButton(colorPickerButton).Content).Kind = PackIconKind.Eyedropper;
+                colorPickerButton = -1;
+                PickStop(); 
+            }
+        }
+
+        private void UserControl_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (colorPickerButton != -1)
+            {
+                ((PackIcon)getButton(colorPickerButton).Content).Kind = PackIconKind.Eyedropper;
+                colorPickerButton = -1;
+                PickStop();
+            }
+        }
+
+        public void SendColor(Color c)
+        {
+            getButton(lastPicker).Background = new SolidColorBrush(c);
+            colors[lastPicker] = c;
+            set[lastPicker] = true;
+
+            ((PackIcon)getButton(colorPickerButton).Content).Kind = PackIconKind.Tick;
+            colorPickerButton = -1;
+            PickStop();
+        }
+    }
+}
--- ImageToMidi/MainWindow.xaml.cs
+++ ImageToMidi/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,6 +12,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +24,179 @@ namespace ImageToMidi
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static readonly DependencyProperty FadeInStoryboard =
+            DependencyProperty.RegisterAttached("FadeInStoryboard", typeof(Storyboard), typeof(MainWindow), new PropertyMetadata(default(Storyboard)));
+        public static readonly DependencyProperty FadeOutStoryboard =
+            DependencyProperty.RegisterAttached("FadeOutStoryboard", typeof(Storyboard), typeof(MainWindow), new PropertyMetadata(default(Storyboard)));
+
+        bool leftSelected = true;
+
+        byte[] openedImagePixels = null;
+        int openedImageWidth = 0;
+        int openedImageHeight = 0;
+        string openedImagePath = "";
+
+        bool colorPick = false;
+
+        void MakeFadeInOut(DependencyObject e)
+        {
+            DoubleAnimation fadeIn = new DoubleAnimation();
+            fadeIn.From = 0.0;
+            fadeIn.To = 1.0;
+            fadeIn.Duration = new Duration(TimeSpan.FromSeconds(0.2));
+
+            Storyboard fadeInBoard = new Storyboard();
+            fadeInBoard.Children.Add(fadeIn);
+            Storyboard.SetTarget(fadeIn, e);
+            Storyboard.SetTargetProperty(fadeIn, new PropertyPath(Rectangle.OpacityProperty));
+
+            e.SetValue(FadeInStoryboard, fadeInBoard);
+
+            DoubleAnimation fadeOut = new DoubleAnimation();
+            fadeOut.From = 1.0;
+            fadeOut.To = 0.0;
+            fadeOut.Duration = new Duration(TimeSpan.FromSeconds(0.2));
+
+            Storyboard fadeOutBoard = new Storyboard();
+            fadeOutBoard.Children.Add(fadeOut);
+            Storyboard.SetTarget(fadeOut, e);
+            Storyboard.SetTargetProperty(fadeOut, new PropertyPath(Rectangle.OpacityProperty));
+
+            e.SetValue(FadeOutStoryboard, fadeOutBoard);
+        }
+
+        void TriggerMenuTransition(bool left)
+        {
+            if (left)
+            {
+                ((Storyboard)selectedHighlightRight.GetValue(FadeOutStoryboard)).Begin();
+                ((Storyboard)selectedHighlightLeft.GetValue(FadeInStoryboard)).Begin();
+                tabSelect.SelectedIndex = 0;
+            }
+            else
+            {
+                ((Storyboard)selectedHighlightRight.GetValue(FadeInStoryboard)).Begin();
+                ((Storyboard)selectedHighlightLeft.GetValue(FadeOutStoryboard)).Begin();
+                tabSelect.SelectedIndex = 1;
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            MakeFadeInOut(selectedHighlightLeft);
+            MakeFadeInOut(selectedHighlightRight);
+            MakeFadeInOut(colPickerHint);
+
+            colPicker.PickStart += ColPicker_PickStart;
+            colPicker.PickStop += ColPicker_PickStop;
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            double start = windowContent.ActualWidth;
+            windowContent.Width = start;
+            for (double i = 1; i > 0; i -= 0.05)
+            {
+                double smooth;
+                double strength = 10;
+                if (i < 0.5f)
+                {
+                    smooth = Math.Pow(i * 2, strength) / 2;
+                }
+                else
+                {
+                    smooth = 1 - Math.Pow((1 - i) * 2, strength) / 2;
+                }
+                Width = start * smooth;
+                Thread.Sleep(1000 / 60);
+            }
+            Close();
+        }
+
+        private void RawMidiSelect_Click(object sender, RoutedEventArgs e)
+        {
+            if (!leftSelected)
+                TriggerMenuTransition(true);
+            leftSelected = true;
+        }
+
+        private void ColorEventsSelect_Click(object sender, RoutedEventArgs e)
+        {
+            if (leftSelected)
+                TriggerMenuTransition(false);
+            leftSelected = false;
+        }
+
+        private void BrowseImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Common image files (*.png;*.jpg;*.jpeg;*.bmp)|*.png;*.jpg;*.jpeg;*.bmp";
+            if (!(bool)open.ShowDialog()) return;
+            openedImagePath = open.FileName;
+            BitmapImage src = new BitmapImage();
+            src.BeginInit();
+            src.UriSource = new Uri(openedImagePath);
+            src.CacheOption = BitmapCacheOption.OnLoad;
+            src.EndInit();
+            openedImageWidth = src.PixelWidth;
+            openedImageHeight = src.PixelHeight;
+            if (openedImageWidth != 128 && openedImageWidth != 256)
+            {
+                MessageBox.Show("The image must be either 128 or 256 pixels wide\n(For midi keyboard)", "Incorrect format");
+                return;
+            }
+            int stride = src.PixelWidth * 4;
+            int size = src.PixelHeight * stride;
+            openedImagePixels = new byte[size];
+            src.CopyPixels(openedImagePixels, stride, 0);
+            openedImage.Source = src;
+        }
+
+        private void OpenedImage_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!colorPick)
+            {
+                if (openedImagePath == "") return;
+                Process imageView = new Process();
+                imageView.StartInfo.FileName = openedImagePath;
+                imageView.Start();
+            }
+            else
+            {
+                var pos = e.GetPosition(openedImage);
+                int x = (int)Math.Round(pos.X / openedImage.ActualWidth * openedImageWidth);
+                int y = (int)Math.Round(pos.Y / openedImage.ActualHeight * openedImageHeight);
+                if (x < 0) x = 0;
+                if (x >= openedImageWidth) x = openedImageWidth - 1;
+                if (y < 0) y = 0;
+                if (y >= openedImageHeight) y = openedImageHeight - 1;
+
+                int s = openedImageWidth * 4;
+
+                Color c = Color.FromArgb(255, openedImagePixels[s * y + x * 4 + 2], openedImagePixels[s * y + x * 4 + 1], openedImagePixels[s * y + x * 4 + 0]);
+                colPicker.SendColor(c);
+            }
+        }
+
+        private void MinimiseButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void ColPicker_PickStart()
+        {
+            Cursor = Cursors.Cross;
+            ((Storyboard)colPickerHint.GetValue(FadeInStoryboard)).Begin();
+            colorPick = true;
+        }
+
+        private void ColPicker_PickStop()
+        {
+            Cursor = Cursors.Arrow;
+            ((Storyboard)colPickerHint.GetValue(FadeOutStoryboard)).Begin();
+            colorPick = false;
         }
     }
 }
